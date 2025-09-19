330cf7322c86a1f13b454554754222a708069964
Vicente Mattos <vicentemattosf@gmail.com>
2025-09-19 19:04:03 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_should_return_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.json() == {'message': 'Hello world'}
+    assert response.status_code() == HTTPStatus.OK
